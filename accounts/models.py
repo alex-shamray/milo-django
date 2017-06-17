2ba9165c3fae550de54f06c8ebe8ea1c1af7b804
Alex Shamray <oleksiy.shamray@gmail.com>
2017-06-17 15:35:44 +0300

    Custom authentication model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+def randint():
+    return random.randrange(1, 101)
+
+
+class User(AbstractUser):
+    """
+    Users within the Django authentication system are represented by this
+    model.
+
+    Username, password and email are required. Other fields are optional.
+    """
+
+    birthday = models.DateField(_('birthday'), blank=True, null=True)
+    random_num = models.IntegerField(_('random number'), default=randint)
