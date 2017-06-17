e6a197ab7e8291f12999d1c41159411a05954cd4
Alex Shamray <oleksiy.shamray@gmail.com>
2017-06-18 02:15:12 +0300

    Regitered is_allowed and bizz_fuzz tags

--- accounts/templatetags/accounts.py
+++ accounts/templatetags/accounts.py
@@ -1,3 +1,13 @@
 from django import template
 
 register = template.Library()
+
+
+@register.tag
+def is_allowed(parser, token):
+    pass
+
+
+@register.tag
+def bizz_fuzz(parser, token):
+    pass
