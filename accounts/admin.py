694612af2f1f09c337b3b87646d2b4a022e462b5
Alex Shamray <oleksiy.shamray@gmail.com>
2017-06-19 11:22:52 +0300

    Admin panel for users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib import auth
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(auth.admin.UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'birthday')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'birthday', 'random_num')
