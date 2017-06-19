51953bda2ab81805258d54b9d00d30cc808dc658
Alex Shamray <oleksiy.shamray@gmail.com>
2017-06-19 11:26:24 +0300

    Registered auth URLs

--- milo/urls.py
+++ milo/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url('^', include('django.contrib.auth.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', admin.site.urls),
 ]
